90a7ca8990b27e77d6da8488795d9f94b9eaa282
Timo Prüße <chroma91@gmail.com>
2020-07-18 00:38:34 +0200

    set up example structure

---
+++ example/src/example.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Example = () => {
+  return <div>example</div>
+}
+
+export default React.memo(Example)
---
+++ example/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      'react-conversation': path.resolve(__dirname, '../dist'),
+    },
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'example.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+}
